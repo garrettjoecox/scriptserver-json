90287bfa96394afc94b020a1dabc206cad3db94b
Garrett Cox <garrettjcox@gmail.com>
2017-10-16 19:22:38 -0500

    Update for 1.0.0

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
-'use strict';
 
 const fs = require('fs');
-const join = require('path').join;
+const { join } = require('path');
+const defaultsDeep = require('lodash.defaultsdeep');
 
-module.exports = function() {
-
-  // Setup
-
+module.exports = function () {
   const server = this;
-  const dir = join(process.cwd(), 'JSON');
-
-  fs.stat(dir, err => {
-    if (err) fs.mkdir(dir);
+  const config = server.config.json = defaultsDeep({}, server.config.json, {
+    dir: join(process.cwd(), 'JSON'),
   });
 
-  // Retrieve & Setting
+  fs.stat(config.dir, (err) => {
+    if (err) fs.mkdir(config.dir);
+  });
 
   server.JSON = {
-
     get(path, attr) {
-      path = join(dir, path.toLowerCase() + '.json');
+      path = join(config.dir, path.toLowerCase() + '.json');
 
       return readJson(path)
         .then(data => attr ? data[attr] : data);
     },
 
     set(path, attr, value) {
-      path = join(dir, path.toLowerCase() + '.json');
+      path = join(config.dir, path.toLowerCase() + '.json');
 
       return readJson(path)
-        .then(data => {
+        .then((data) => {
           data[attr] = value;
           return writeJson(path, data);
         });
-    }
-
-  }
-}
-
-// Helper Functions
+    },
+  };
+};
 
 function readJson(path) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     fs.readFile(path, (err, data) => {
       if (err) resolve({});
       else resolve(JSON.parse(data));
@@ -51,7 +44,7 @@ function readJson(path) {
 
 function writeJson(path, data) {
   return new Promise((resolve, reject) => {
-    fs.writeFile(path, JSON.stringify(data, null, 4), err => {
+    fs.writeFile(path, JSON.stringify(data, null, 4), (err) => {
       if (err) reject(err);
       else resolve(data);
     });
